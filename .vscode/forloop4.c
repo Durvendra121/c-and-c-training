9004460932e4835a4ed19c8081dec40d1a36ee53
Durvendra121 <durvendrathakur340@gmail.com>
2025-09-11 12:35:26 +0530

    x star pattern

---
+++ .vscode/forloop4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n=5;
+    for(int i=0; i<=4; i++){
+        for(int j=0; j<=4; j++){
+           // printf("*");
+           if (i==j ||j==(4-i)){
+               printf("*");
+           }
+           else{
+               printf(" ");
+           }
+        }
+        printf("\n");
+          
+    }
+}
