7861c45381fa4777a4384b0d0d098b79df39ba96
Durvendra121 <durvendrathakur340@gmail.com>
2025-09-15 13:42:42 +0530

    printing of array

---
+++ c training/array01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    //int a = 2;
+    //int b = 5;
+
+
+int arr[5]={1,2,3,4,5};
+//printf("%d",arr[1]);
+for (int i=4; i>=0;i--){
+    printf("%d",arr[i]);    
+    printf("\n");
+}
+return 0;
+}
