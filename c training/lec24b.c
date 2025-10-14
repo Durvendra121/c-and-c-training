516c285d836b560710fc643c21c843ab40b13142
Durvendra121 <durvendrathakur340@gmail.com>
2025-10-14 13:59:31 +0530

    struct

--- c training/lec24b.c
+++ c training/lec24b.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+struct address{
+    int pincode ;
+};
 struct student 
 {
     char name [50];
     int roll_no;
     int age;
+    struct address S2;;
 };
  int main()
  {
 struct student s1;
-//s1 = {"Durvendra", 101, 21};
+s1.S2.pincode = 123456;
 s1.age=21;
-printf("%d",s1.age);
+printf("PINCODE IS:%d",s1.S2.pincode);
+printf("\nAGE IS :%d",s1.age);
 
 }
+
